ab90a5f727afe83cb676a56053ef561a07a51527
s36491 <s36491@DEP266>
2018-11-02 14:55:37 +0300

    Added logging dll

---
+++ ProcessLog/Log.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ProcessLog
+{
+    public static class Log
+    {
+        
+        public static string LogFileName;
+        
+
+
+        public static void Init(string fileName)
+        {
+            LogFileName = fileName;
+
+            File.WriteAllText(fileName,);
+        }
+
+        public static void Add(string line)
+        {
+            
+        }
+
+        private static string getCurrentTime()
+        {
+            return string.Format("{0}:{1}:{2}", DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+        }
+
+        private static string getCurrentDate()
+        {
+            return string.Format("{0}/{1}/{2}", DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
+        }
+    }
+}
